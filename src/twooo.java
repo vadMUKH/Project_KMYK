764c1c20c87e1a91f1e7c03bfac7888b7aba8761
Vadim <nuvare1@gmail.com>
2020-06-10 23:06:17 +0500

    Добавили информацию о GitHub- е в кнопку "информация"

--- src/twooo.java
+++ src/twooo.java
@@ -14,7 +14,7 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class twooo extends JFrame {
-private JLabel fbo = new JLabel("Для нового класса");
+private JLabel fbo = new JLabel("GitHub — крупнейший веб-сервис для хостинга IT-проектов и их совместной разработки.Веб-сервис основан на системе контроля версий Git и разработан на Ruby on Rails и Erlang компанией GitHub, Inc (ранее Logical Awesome). Сервис бесплатен для проектов с открытым исходным кодом и (с 2019 года) небольших частных проектов, предоставляя им все возможности (включая SSL), а для крупных корпоративных проектов предлагаются различные платные тарифные планы.");
 public twooo() {
 super("Window");
 this.setBounds(300,600,420,300);
